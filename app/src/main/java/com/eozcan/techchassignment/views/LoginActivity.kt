05e9163bf4e87e97f5cef1043aaaf7d6227bff4a
eozcan <eozcan@invio.com>
2019-09-21 22:58:19 +0300

    Login sayfasında beni hatırla akışı yapıldı.

--- app/src/main/java/com/eozcan/techchassignment/views/LoginActivity.kt
+++ app/src/main/java/com/eozcan/techchassignment/views/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.eozcan.techchassignment.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -17,19 +18,30 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // Kullanıcı beni hatırlayı seçmişse siparişlerim sayfasına geçiş
+        if (SharedPreferencesUtil.getBoolean(this, Params.KEY_REMEMBER_ME)) {
+            startOrdersActivity()
+            return
+        }
+
         val binding =
             DataBindingUtil.setContentView<ActivityLoginBinding>(this, R.layout.activity_login)
         // viewModel'den activity başlatmak için interface tanımlayıp kullandım.
         binding.viewModel = LoginViewModel(object : LoginListener {
             override fun onLogicSucceded(rememberMeActive: Boolean) {
                 SharedPreferencesUtil.setBoolean(this@LoginActivity, Params.KEY_REMEMBER_ME, rememberMeActive)
-                // TODO start ordersActivity
+                startOrdersActivity()
             }
 
             override fun onLoginFailed() {
                 AlertUtil.showAlert(this@LoginActivity, R.string.app_name, R.string.login_fail_warning, R.string.ok)
             }
         })
+    }
 
+    private fun startOrdersActivity() {
+        val intent = Intent(this@LoginActivity, OrdersActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
