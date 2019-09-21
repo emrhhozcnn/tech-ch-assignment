e2aa41f2c091ad054c3a29a4358fc195338ced23
eozcan <eozcan@invio.com>
2019-09-21 23:53:01 +0300

    Siparişlerim sayfasında çekilen siparişler adapter'e set edildi.

--- app/src/main/java/com/eozcan/techchassignment/viewmodels/OrdersViewModel.kt
+++ app/src/main/java/com/eozcan/techchassignment/viewmodels/OrdersViewModel.kt
@@ -2,8 +2,25 @@ package com.eozcan.techchassignment.viewmodels
 
 import androidx.databinding.BaseObservable
 import com.eozcan.techchassignment.adapters.OrdersAdapter
+import com.eozcan.techchassignment.models.Order
+import com.eozcan.techchassignment.network.NetworkClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class OrdersViewModel : BaseObservable() {
     val adapter = OrdersAdapter()
 
+    init {
+        NetworkClient.getService().getOrderList().enqueue(object : Callback<List<Order>> {
+            override fun onResponse(call: Call<List<Order>>, response: Response<List<Order>>) {
+                adapter.orderList = response.body() as ArrayList<Order>
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<List<Order>>, t: Throwable) {
+                // TODO
+            }
+        })
+    }
 }
