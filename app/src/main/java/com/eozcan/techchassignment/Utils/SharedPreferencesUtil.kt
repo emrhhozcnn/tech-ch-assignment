4bf4039e6ae6435abb8ad23857cd05a5efc5e82c
eozcan <eozcan@invio.com>
2019-09-21 20:47:12 +0300

    Cihaza veri kaydetmek için sharedPrereferences util dosyası geliştirildi.

---
+++ app/src/main/java/com/eozcan/techchassignment/Utils/SharedPreferencesUtil.kt
@@ -0,0 +1,20 @@
+package com.eozcan.techchassignment.Utils
+
+import android.content.Context
+
+class SharedPreferencesUtil {
+    companion object {
+        private val PREF_FNAME = ".session"
+        fun setBoolean(context: Context, key: String, value: Boolean) {
+            val sharedPref = context.getSharedPreferences(PREF_FNAME, Context.MODE_PRIVATE)
+            val editor = sharedPref.edit()
+            editor.putBoolean(key, value)
+            editor.apply()
+        }
+
+        fun getBoolean(context: Context, key: String): Boolean {
+            val sharedPref = context.getSharedPreferences(PREF_FNAME, Context.MODE_PRIVATE)
+            return sharedPref.getBoolean(key, false)
+        }
+    }
+}
