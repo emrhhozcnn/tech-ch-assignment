551373e3373ce20ff5edf167d3fdf934b09b1a10
eozcan <eozcan@invio.com>
2019-09-21 23:47:54 +0300

    Siparişlerim sayfasında view ile viewModel bağlandı.

--- app/src/main/java/com/eozcan/techchassignment/views/OrdersActivity.kt
+++ app/src/main/java/com/eozcan/techchassignment/views/OrdersActivity.kt
@@ -2,12 +2,19 @@ package com.eozcan.techchassignment.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.eozcan.techchassignment.R
+import com.eozcan.techchassignment.databinding.ActivityOrdersBinding
+import com.eozcan.techchassignment.viewmodels.OrdersViewModel
 
 class OrdersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_orders)
+
+        val binding =
+            DataBindingUtil.setContentView<ActivityOrdersBinding>(this, R.layout.activity_orders)
+        binding.viewModel = OrdersViewModel()
     }
 }
