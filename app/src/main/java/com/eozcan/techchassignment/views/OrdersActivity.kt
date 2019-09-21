0d90858e169525fa75b0acbce3fd8d9512ee8603
eozcan <eozcan@invio.com>
2019-09-21 22:52:47 +0300

    Siparişlerim sayfası oluşturuldu.

---
+++ app/src/main/java/com/eozcan/techchassignment/views/OrdersActivity.kt
@@ -0,0 +1,13 @@
+package com.eozcan.techchassignment.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eozcan.techchassignment.R
+
+class OrdersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_orders)
+    }
+}
