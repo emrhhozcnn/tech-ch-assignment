181174dd3895c3520a9a5975376967ff67032a3d
eozcan <eozcan@invio.com>
2019-09-21 20:26:05 +0300

    Login sayfasında view - modelView bağlantısı kuruldu.

--- app/src/main/java/com/eozcan/techchassignment/views/LoginActivity.kt
+++ app/src/main/java/com/eozcan/techchassignment/views/LoginActivity.kt
@@ -2,12 +2,20 @@ package com.eozcan.techchassignment.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.eozcan.techchassignment.R
+import com.eozcan.techchassignment.databinding.ActivityLoginBinding
+import com.eozcan.techchassignment.viewmodels.LoginViewModel
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val binding =
+            DataBindingUtil.setContentView<ActivityLoginBinding>(this, R.layout.activity_login)
+        binding.viewModel = LoginViewModel()
+
     }
 }
